21eb526a5ea75239cca8c8380dc44b281b946c74
adrianvilla1504 <adriancvilla@gmail.com>
2022-09-11 14:35:20 -0500

    feat: add to routes file auth local, modify index file, and git ignore .env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const DBconnection = require()
 
 const app = express();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, async () => {
   expressConfiguration(app);
--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 const router = require('./api/healthcheck/index');
-const favs = require('.api/favs/index');
+const fav = require('.api/favs/index');
+const auth = require('./auth/local');
 
 function routes(app) {
   app.use('/api/healthcheck', router);
   app.use('/api/favs', fav);
+  app.use('/auth/local', auth);
 
 }
 
