b2ba69eb2295a50d597649a892f1a2e5a3bfeca3
adrianvilla1504 <adriancvilla@gmail.com>
2022-09-13 13:29:58 -0500

    feat: fix auth route space bug

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
 const router = require('./api/healthcheck/index');
-const fav = require('.api/favs/index');
-const auth = require('./auth/local');
+const favs = require('./api/favs/index');
+const auth = require('./auth/local/index');
 
 function routes(app) {
   app.use('/api/healthcheck', router);
-  app.use('/api/favs', fav);
+  app.use('/api/favs', favs);
   app.use('/auth/local', auth);
 
 }
